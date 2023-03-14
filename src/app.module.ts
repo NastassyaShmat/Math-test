0a294af2d35515006c1b9c6b326c032b1cd423dd
anastasia.shkutovich <anastasia.shkutovich@succraft.com>
2023-03-14 11:37:45 +0300

    add resources.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { QuestionsModule } from './questions/questions.module';
+import { AnswersModule } from './answers/answers.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule, QuestionsModule, AnswersModule],
   controllers: [AppController],
   providers: [AppService],
 })
