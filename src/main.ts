f5760a940fa1316040d7bdd8825d9597ac2b2c0f
anastasia.shkutovich <anastasia.shkutovich@succraft.com>
2023-03-14 12:07:41 +0300

    add swagger docs.

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  
+  const config = new DocumentBuilder()
+  .setTitle('Math Test App Server')
+  .setDescription('Math Test App Server API description')
+  .setVersion('1.0')
+  .addTag('Users')
+  .addTag('Questions')
+  .addTag('Answers')
+  .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
